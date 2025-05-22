b9d4f9d29e4ced664e46f22a3aaa450b33b5e331
karthi0205 <pintud2515@gmail.com>
2025-05-22 13:10:06 +0530

    Add files via upload

---
+++ lesson4/l4activity1.py
@@ -0,0 +1,10 @@
+a=23
+b=34
+c=45
+d=56
+e=67
+
+sum=a+b+c+d+e
+print("the sum of all numbers: ",sum)
+ave=sum/5
+print("the average of all numbers: ",ave)
---
+++ lesson4/l4activity3.py
@@ -0,0 +1,11 @@
+print("enter the marks from hundred")
+maths=int(input("maths:" ))
+Sci=int(input("Sci:"))
+eng=int(input("eng:"))
+tel=int(input("tel:"))
+
+sum=maths+Sci+eng+tel
+print("this is the sum of all subjects ",sum)
+
+per=(sum/400)*100
+print("this is the percentage of all subjects ",per)
