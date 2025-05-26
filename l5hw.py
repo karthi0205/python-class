40cfe867c92ffa01c6246d5adc4495c400548988
karthi0205 <pintud2515@gmail.com>
2025-05-26 13:37:37 +0530

    Add files via upload

---
+++ l5hw.py
@@ -0,0 +1,9 @@
+temperature = float(input("Enter the temperature in °C: "))
+
+if temperature > 25:
+    print("Wear light and soft clothes.")
+elif temperature >= 15:
+    print("It's a bit cool. Maybe wear a light jacket.")
+else:
+    print("It's cold. Wear a jacket or pullover.")
+
