c19cfb2e0a0abab75559cfa84d0eaff1ccb063f0
karthi0205 <pintud2515@gmail.com>
2025-05-27 12:54:28 +0530

    Add files via upload

---
+++ lesson6/l6activity1.py
@@ -0,0 +1,14 @@
+a=12
+s=23
+d=45
+f=87
+sum=(s+d)*(a+f)
+print("value of sum",sum)
+
+name="prachi mam "
+age=10
+if name=="karthi" or name=="prachi mam " and age>=12:
+    print("hi")
+
+else:
+    print("good")
